664c63bf076dfcaa930fa3c8329ed040558f0c9a
Spencer Smitherman <spencer.smitherman@gmail.com>
2015-02-09 21:40:55 -0800

    Added routes to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Spencer' });
-});
+module.exports = function(passport){
+
+	/* GET home page. */
+	router.get('/', function(req, res) {
+	  res.render('index', { title: 'Spencer' });
+	});
+
+	/* Handle Login POST */
+	router.post('/login', passport.authenticate('login', {
+		successRedirect: '/home',
+		failureRedirect: '/',
+		failureFlash: true
+	}));
+
+	/* GET Registration Page */
+	router.get('/signup', function(req, res){
+		res.render('register', {message: req.flash('message')});
+	});
+
+	/* Handle Registraion POST */
+	router.post('/signup', passport.authenticate('signup', {
+		successRedirect: '/home',
+		failureRedirect: '/signup',
+		failureFlash: true
+	}));
+
+	return router;
+}
 
 module.exports = router;
